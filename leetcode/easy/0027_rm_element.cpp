97a16ce3e1930ec30225c07f85aa3277ade46ca6
Юрий Спиридонов <kaktus20110@yandex.com>
2026-03-28 15:48:03 +0300

    feat: solve Remove Element using two pointers

---
+++ leetcode/easy/0027_rm_element.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+  int removeElement(std::vector<int> &nums, int val);
+};
+
+int Solution::removeElement(std::vector<int> &nums, int val) {
+  if (nums.empty())
+    return 0;
+  int start = 0;
+  int end = nums.size() - 1;
+  while (start <= end) {
+    if (nums[start] == val && nums[end] != val) {
+      nums[start] = nums[end];
+      start++;
+      end--;
+    } else if (nums[end] == val) {
+      end--;
+    } else {
+      start++;
+    }
+  }
+
+  return start;
+}
