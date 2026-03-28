67ec2ca5c5c2bf41b1e5b8775df4e12e55811880
Юрий Спиридонов <kaktus20110@yandex.com>
2026-03-28 14:54:10 +0300

    fix: add missing std:: for vector

--- leetcode/easy/0001_two_sum.cpp
+++ leetcode/easy/0001_two_sum.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-  vector<int> twoSum(vector<int> &nums, int target);
+  std::vector<int> twoSum(std::vector<int> &nums, int target);
 };
 
-vector<int> Solution::twoSum(vector<int> &nums, int target) {
+std::vector<int> Solution::twoSum(std::vector<int> &nums, int target) {
   for (int i = 0; i < nums.size(); ++i) {
     for (int j = i + 1; j < nums.size(); ++j) {
       if (nums[i] + nums[j] == target) {
-        return vector<int> ans{nums[i], nums[j]};
+        return std::vector<int>{i, j};
       }
     }
   }
