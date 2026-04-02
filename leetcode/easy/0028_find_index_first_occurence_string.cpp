6224836fc2486fe35de9be33636d68b5ec8b1f9f
Юрий Спиридонов <kaktus20110@yandex.com>
2026-04-02 13:12:52 +0300

    feat: solve find index occurence string using two pointers and brute-force

---
+++ leetcode/easy/0028_find_index_first_occurence_string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+  int strStr(std::string haystack, std::string needle);
+};
+
+int Solution::strStr(std::string haystack, std::string needle) {
+  if (needle.empty())
+    return 0;
+
+  int p1 = 0;
+  int p2 = 0;
+
+  while (p1 < haystack.length()) {
+    if (haystack[p1] == needle[p2]) {
+      p1++;
+      p2++;
+      if (p2 == needle.length()) {
+        return p1 - needle.length();
+      }
+    } else {
+      p1 = p1 - p2 + 1;
+      p2 = 0;
+    }
+  }
+  return -1;
+}
