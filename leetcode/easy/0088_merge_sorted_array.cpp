11906061bb44277935a26149297ba8c3c4e87f05
Юрий Спиридонов <kaktus20110@yandex.com>
2026-04-02 11:23:48 +0300

    fix: add missing std:: for vector

--- leetcode/easy/0088_merge_sorted_array.cpp
+++ leetcode/easy/0088_merge_sorted_array.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-  void merge(vector<int> &nums1, int m, vector<int> &nums2, int n);
+  void merge(std::vector<int> &nums1, int m, std::vector<int> &nums2, int n);
 };
 
-void Solution::merge(vector<int> &nums1, int m, vector<int> &nums2, int n) {
+void Solution::merge(std::vector<int> &nums1, int m, std::vector<int> &nums2, int n) {
   int p1 = m - 1;
   int p2 = n - 1;
   int p3 = m + n - 1;
