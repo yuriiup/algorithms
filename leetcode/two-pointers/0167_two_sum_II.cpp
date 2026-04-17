18aa721cb1d6463ac9e27e679d55c523e74f69db
Юрий Спиридонов <yurriiup@yandex.com>
2026-04-17 21:49:21 +0300

    feat: add two_sum_II using two pointers

---
+++ leetcode/two-pointers/0167_two_sum_II.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> twoSum(std::vector<int>& numbers, int target);
+};
+
+std::vector<int> Solution::twoSum(std::vector<int>& numbers, int target) {
+  int start = 0;
+  int end = numbers.size() - 1;
+
+  long number;
+  while (start < end) {
+    number = numbers[start] + numbers[end];
+    if (number > target) {
+      end--;
+    } else if (number < target) {
+      start++;
+    } else {
+      return std::vector<int>{start + 1, end + 1};
+    }
+  }
+  return {};
+}
